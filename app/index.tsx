bd4749d9e18a14169bf698f7889daa512e8addc8
Christian Andreas Voß <vosschri@uni-marburg.de>
2025-01-04 20:17:32 +0100

    added back the missing router functionality of index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View, ScrollView, StyleSheet  } from "react-native";
+import { View, ScrollView, StyleSheet  } from "react-native";
 import PlusButton from "@/components/PlusButton";
 import DataInputField from "@/components/DataInputField";
 import GlobalStyles from "@/styles/globalStyles";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
+  const onPlusPress = () => {
+    router.push("/varChooser");
+  }
   return (
     <View
       style={[styles.container, GlobalStyles.backgroundColor, GlobalStyles.container]}
     >
-      <ScrollView >
-        <PlusButton onPress={() => { }} /> {/* following for Test purposes */}
+      <ScrollView>
+        {/* Plus Button to start new Table setup (link to varChooser) */}
+        <PlusButton onPress={onPlusPress} />
+
+        {/* following for Test purposes */}
         <DataInputField paramName=" !Test: Schuhgröße" />
         
       </ScrollView>
