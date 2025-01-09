e7a15e95587bd2380c80795705f2c3c1a27f6c28
Christian Andreas Voß <vosschri@uni-marburg.de>
2025-01-09 14:11:12 +0100

    applied global style on dataInput.tsx

--- app/dataInput.tsx
+++ app/dataInput.tsx
@@ -1,5 +1,7 @@
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
+import GlobalStyles from "@/styles/globalStyles";
 
 import { DataBase } from "@/services/database";
 import { useEffect } from "react";
@@ -27,7 +29,9 @@ export default function DataInput() {
     useEffect(() => {loadDataInputFields();}); // this method is run when the page is loaded
 
     return (
-        <View>
+        <View
+            style={[styles.container, GlobalStyles.backgroundColor, GlobalStyles.container]}
+        >
             {/* MAIN CONTENT VIEW */}
             <View>
                 <Text>Table name: {tableName}</Text>
@@ -38,3 +42,9 @@ export default function DataInput() {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
